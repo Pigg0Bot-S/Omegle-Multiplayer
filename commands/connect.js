8bb7595ae29466e5dca8e89d474b4a7ee16ffb87
Pigg0Bot <cole.thomas.leggett@gmail.com>
2022-12-12 23:26:39 -0500

    Fix Critical Bugs!

--- commands/connect.js
+++ commands/connect.js
@@ -4,26 +4,39 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('connect')
         .setDescription('Connects channel to Omegle')
-        .addBooleanOption(option =>
+        .addIntegerOption(option =>
             option
                 .setName("groupchat")
-                .setDescription("Whether to make it a group chat or not.")),
+                .setDescription("Whether to make it a group chat or not.")
+                .addChoices(
+                    {name: "auto", value: 0},
+                    {name: "stranger", value: 1},
+                    {name: "group", value: 2},
+                )),
     async execute(interaction) {
-        const groupChat = interaction.options.getBoolean('groupchat');
+        let groupChat = interaction.options.getInteger('groupchat');
+        if (groupChat === null) groupChat = 0;
         if (Object.keys(perms).includes(interaction.channelId)) {
             if (perms[interaction.channel.id] === 0) {
                 interaction.reply("Omegle commands are disabled in this channel.");
                 return;
-            } else if (perms[interaction.channel.id] === 1) {
-                if (groupChat) {
+            } else if (groupChat === 2) {
+                if (perms[interaction.channel.id] === 1) {
                     interaction.reply("Group chats are disabled in this channel.");
                     return;
                 }
-            } else if (perms[interaction.channel.id] === 2) {
-                if (!groupChat) {
+                groupChat = true;
+            } else if (groupChat === 1) {
+                if (perms[interaction.channel.id] === 2) {
                     interaction.reply("Group chats are required in this channel.");
                     return;
                 }
+                groupChat = false;
+            } else if (groupChat === 0) {
+                if (perms[interaction.channel.id] === 3) groupChat = false;
+                else if (perms[interaction.channel.id] === 2) groupChat = true;
+                else groupChat = false;
+
             }
         } else {
             interaction.reply("Omegle commands are disabled in this channel.");
@@ -33,6 +46,7 @@ module.exports = {
             await interaction.reply("This channel is already connected to Omegle!");
             return;
         }
+        console.log(groupChat);
         activeSessions[interaction.channel.id] = new OmegleClient(interaction.channel, groupChat);
         await activeSessions[interaction.channel.id].connect();
         if (activeSessions[interaction.channel.id].groupchat) {
